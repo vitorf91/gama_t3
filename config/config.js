fd052d0116a1d50edce84fcab25df61c2fb8f7af
Felipe blini <Felipe.blini@gmail.com>
2016-09-17 15:06:18 -0300

    initial commit

---
+++ config/config.js
@@ -0,0 +1,34 @@
+var path = require('path'),
+    rootPath = path.normalize(__dirname + '/..'),
+    env = process.env.NODE_ENV || 'development';
+
+var config = {
+  development: {
+    root: rootPath,
+    app: {
+      name: 'cabify-time3'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/cabify-time3-development'
+  },
+
+  test: {
+    root: rootPath,
+    app: {
+      name: 'cabify-time3'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/cabify-time3-test'
+  },
+
+  production: {
+    root: rootPath,
+    app: {
+      name: 'cabify-time3'
+    },
+    port: 3000,
+    db: 'mongodb://localhost/cabify-time3-production'
+  }
+};
+
+module.exports = config[env];
